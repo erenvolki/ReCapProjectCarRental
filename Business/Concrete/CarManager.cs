c0c7c43dcb811979ea657b8d1ee6f1d5248f6651
Volkan <erenvolki@gmail.com>
2021-02-13 01:56:56 +0300

    Add project files.

---
+++ Business/Concrete/CarManager.cs
@@ -0,0 +1,53 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class CarManager:ICarService
+    {
+        ICarDal _carDal;
+
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int colorId)
+        {
+            return _carDal.GetAll(p => p.ColorId == colorId);
+        }
+
+        public void Add(Car car)
+        {
+            if (car.DailyPrice > 0 && car.Name.Length > 2)
+            {
+                _carDal.Add(car);
+            }
+
+        }
+
+        public void Delete(Car car)
+        {
+            _carDal.Delete(car);
+        }
+
+        public void Update(Car car)
+        {
+            _carDal.Update(car);
+        }
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,84 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.Entityframework;
+using Entities.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            //Console.WriteLine("*******************************************************");
+            //Console.WriteLine("---------------GET ALL---------------------------------");
+            //Console.WriteLine("ID---NAME---MODELYEAR---DAILYPRICE---DESCRIPTION");
+            //Console.WriteLine();
+
+            //foreach (var car in carManager.GetAll())
+            //{
+            //    Console.WriteLine(car.Id + "--" + car.Name + "-------" + car.ModelYear + "-------" + car.DailyPrice + "TL" + "--------" + car.Description);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("---------------GET CARS BY BRAND ID--------------------");
+            //Console.WriteLine("*******************************************************");
+
+            //foreach (var a in carManager.GetCarsByBrandId(3))
+            //{
+            //    Console.WriteLine(a.BrandId + "--" + a.Name);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("---------------GET CARS BY COLOR ID--------------------");
+            //Console.WriteLine("*******************************************************");
+
+            //foreach (var color in carManager.GetCarsByColorId(1))
+            //{
+            //    Console.WriteLine(color.Description);
+            //}
+
+            //Console.WriteLine();
+            //Console.WriteLine("---------------CRUD--------------------");
+            //Console.WriteLine("*******************************************************");
+
+            Car k = new Car { Id = 5, Name = "Corsa", BrandId = 2, ColorId = 1, ModelYear = 2015, DailyPrice = 130, Description = "Benzinli manuel vites" };
+            carManager.Add(k);
+            Console.WriteLine("Kayıt eklendi");
+            Console.WriteLine("-----------------------------------------");
+
+            foreach (var p in carManager.GetAll())
+            {
+                Console.WriteLine(p.Id + "--" + p.Name + "-------" + p.ModelYear + "-------" + p.DailyPrice + "TL" + "--------" + p.Description);
+            }
+
+            Console.WriteLine("-----------------------------------------");
+
+            k.ModelYear = 2016;
+            k.DailyPrice = 140;
+
+
+            carManager.Update(k);
+            Console.WriteLine("Kayıt Güncellendi");
+            foreach (var ca in carManager.GetAll())
+            {
+                Console.WriteLine(ca.Id + "--" + ca.Name + "-------" + ca.ModelYear + "-------" + ca.DailyPrice + "TL" + "--------" + ca.Description);
+            }
+
+            Console.WriteLine("-----------------------------------------");
+            Console.WriteLine("-----------------------------------------");
+            
+            carManager.Delete(k);
+            Console.WriteLine("Kayıt silindi");
+
+            foreach (var sil in carManager.GetAll())
+            {
+                Console.WriteLine(sil.Id + "--" + sil.Name + "-------" + sil.ModelYear + "-------" + sil.DailyPrice + "TL" + "--------" + sil.Description);
+            }
+
+
+
+        }
+    }
+}
